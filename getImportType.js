88f172d8af12cd043d8ce9486d00996b50a99779
kmsheng <kmsh3ng@gmail.com>
2021-08-06 16:14:12 +0800

    add getImportType.js

---
+++ getImportType.js
@@ -0,0 +1,40 @@
+const {
+  IMPORT_TYPE_CONTAINER,
+  IMPORT_TYPE_COMPONENT,
+  IMPORT_TYPE_MIXIN,
+  IMPORT_TYPE_REDUCER,
+  IMPORT_TYPE_MODEL,
+  IMPORT_TYPE_UTIL,
+  IMPORT_TYPE_CONST,
+  IMPORT_TYPE_OTHER
+} = require('./consts')
+
+const getImportType = (node, line) => {
+  if (node.type !== 'ImportDeclaration') {
+    return ''
+  }
+  if (line.includes('/containers')) {
+    return IMPORT_TYPE_CONTAINER
+  }
+  if (line.includes('/components')) {
+    return IMPORT_TYPE_COMPONENT
+  }
+  if (line.includes('/mixins')) {
+    return IMPORT_TYPE_MIXIN
+  }
+  if (line.includes('redux/reducers')) {
+    return IMPORT_TYPE_REDUCER
+  }
+  if (line.includes('/models')) {
+    return IMPORT_TYPE_MODEL
+  }
+  if (line.includes('/utils')) {
+    return IMPORT_TYPE_UTIL
+  }
+  if (line.includes('/consts')) {
+    return IMPORT_TYPE_CONST
+  }
+  return IMPORT_TYPE_OTHER
+}
+
+module.exports = getImportType
